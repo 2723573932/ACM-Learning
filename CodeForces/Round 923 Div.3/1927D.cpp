33e3d573213bf008aacf294193324ba1189c7d7a
2723573932 <32301122@stu.hzcu.edu.cn>
2024-05-29 12:49:25 +0000

    写了点div3 尝试写dp然后失败qwq

--- CodeForces/Round 923 Div.3/1927D.cpp
+++ CodeForces/Round 923 Div.3/1927D.cpp
@@ -1,54 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+long long lst[(int)1e6+7];
 void solve()
 {
     int n;
-    cin >> n;
-    int a[n + 1];
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    int q;
-    cin >> q;
-    int l, r, f = 0;
-    while (q--)
+    cin>>n;
+    for(int i=1;i<=n;i++)
     {
-        int i;
-        f = 0;
-        cin >> l >> r;
-        for (i = l; i < r - r % 4; i += 4)
-        {
-            if (a[i] != a[i + 1])
-            {
-                f = 1;
-                break;
-            }
-            if (a[i + 1] != a[i + 2])
-            {
-                f = 1;
-                i+=1;
-                break;
-            }
-            if (a[i + 2 != a[i + 3]])
-            {
-                f = 1;
-                i+= 2;
-                break;
-            }
-        }
-        for (int v = r - r % 4; v < r; v++)
-            if (a[v] != a[v + 1])
-            {
-                f = 1;
-                i=v;
-                break;
-            }
-        if (f == 1)
-            cout << i << ' ' << i + 1 << '\n';
-        else
-            cout << "-1 -1\n";
+        cin>>lst[i];
     }
-    cout << '\n';
+    vector<int> ans;
+    for(int i=1;i<n;i++)
+    {
+        if(lst[i]!=lst[i+1])
+        ans.push_back(i);
+    }
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int l,r;
+        cin>>l>>r;
+        auto it=lower_bound(ans.begin(),ans.end(),l);
+        if(it==ans.end())
+        {
+            cout<<"-1 -1\n";
+            continue;
+        }
+        int tmp=*it;
+        if(tmp>=l&&tmp+1<=r)
+        cout<<tmp<<' '<<tmp+1<<'\n';
+        else cout<<"-1 -1\n";
+    }
+    cout<<'\n';
+
 }
 int main()
 {
