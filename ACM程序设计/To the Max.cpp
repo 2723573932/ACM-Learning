0ae84cc1adb250c804eeadf1b4aa2c90a5d68829
2723573932 <32301122@stu.hzcu.edu.cn>
2024-06-05 23:36:18 +0800

    写了dp

--- ACM程序设计/To the Max.cpp
+++ ACM程序设计/To the Max.cpp
@@ -1,25 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <climits>
 
-void solve()
+int findMaxSubRectangle(const std::vector<std::vector<int> > &matrix)
 {
-    int n;
-    int mp[n][n];
-    for(int i=0;i<n;i++)
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+
+    int maxSum = INT_MIN;
+
+    for (int left = 0; left < cols; ++left)
     {
-        for(int j=0;j<n;j++)
+        std::vector<int> temp(rows, 0);
+
+        for (int right = left; right < cols; ++right)
         {
-            cin>>mp[i][j];
+            for (int i = 0; i < rows; ++i)
+            {
+                temp[i] += matrix[i][right];
+            }
+
+            int currentSum = 0;
+            int maxEndingHere = 0;
+
+            for (int i = 0; i < rows; ++i)
+            {
+                maxEndingHere = std::max(maxEndingHere + temp[i], temp[i]);
+                currentSum = std::max(currentSum, maxEndingHere);
+            }
+
+            maxSum = std::max(maxSum, currentSum);
         }
     }
+
+    return maxSum;
 }
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int tt = 1;
-    // cin >> tt;
-    while (tt--)
-        solve();
+    int n;
+    std::cin>>n;
+    std::vector<std::vector<int> > matrix(n, std::vector<int>(n, 0));
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            std::cin>>matrix[i][j];
+        }
+    }
+    int maxSum = findMaxSubRectangle(matrix);
+    std::cout << maxSum << std::endl;
+
     return 0;
 }
