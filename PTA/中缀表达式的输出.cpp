526658615890300852bc957f78adbd74e97b3011
fy-j <1289757736@qq.com>
2024-06-13 19:37:35 +0800

    终于解决了这个烂题

--- PTA/中缀表达式的输出.cpp
+++ PTA/中缀表达式的输出.cpp
@@ -1,96 +1,157 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// Function to check if the given binary tree exists
-int isBinaryTreeExists(const vector<string> &preOrder, const vector<string> &inOrder)
+const int maxn = 1e3 + 5;
+map<string, int> map1;
+string num1[maxn], num2[maxn]; 
+struct node
 {
-    unordered_map<char, int> preOrderMap;
-    unordered_map<char, int> inOrderMap;
+    node *left;
+    node *right;
+    string data;
+};
+int fla[4]; 
+string s1, s2;
+char a;
+int p1 = -1, p2 = -1, cnt1 = 1, cnt2 = 1;
 
-    // Creating a map for pre-order elements
-    for (int i = 0; i < preOrder.size(); i++)
+node *tree(int l1, int r1, int l2, int r2)
+{
+    node *p1;
+    fla[3] = 0; 
+    if (l1 > r1 && l2 > r2)
     {
-        preOrderMap[preOrder[i][0]]++;
+        return NULL;
     }
-
-    // Creating a map for in-order elements
-    for (int i = 0; i < inOrder.size(); i++)
+    string head = num1[l1];
+    p1 = new node();
+    p1->data = head;
+    for (int i = l2; i <= r2; i++)
     {
-        inOrderMap[inOrder[i][0]]++;
-    }
-
-    // Checking if the number of pre-order elements is different from the number of in-order elements
-    if (preOrderMap.size() != inOrderMap.size())
-    {
-        return 1;
-    }
-
-    // Checking if the pre-order elements and in-order elements are consistent
-    for (int i = 0; i < preOrder.size(); i++)
-    {
-        if (preOrder[i] != inOrder[i])
+        if (num2[i] == head)
         {
-            return 2;
+            p1->left = tree(l1 + 1, l1 + (i - l2), l2, i - 1);
+            p1->right = tree(l1 + (i - l2) + 1, r1, i + 1, r2);
+            fla[3] = 1;
         }
     }
-
-    return 0;
+    if (fla[3] == 0)
+    {
+        fla[2] = 1;
+    }
+    return p1;
 }
 
-// Function to convert the given binary tree into an expression
-string convertToExpression(const vector<string> &preOrder, const vector<string> &inOrder)
+void readd()
 {
-    switch (isBinaryTreeExists(preOrder, inOrder))
+    int tmp;
+    for (int i = 0; i < s1.size(); i++)
     {
-        case 1:
-            return "The number of pre-order elements is different from the number of in-order elements.";
-        case 2:
-            return "Inconsistent pre-order elements and in-order elements.";
-        default:
-            break;
-    }
-
-    stack<string> st;
-
-    for (int i = preOrder.size() - 1; i >= 0; i--)
-    {
-        if (isalpha(preOrder[i][0]))
+        if (s1[i] == ' ')
         {
-            st.push(string(1, preOrder[i][0]));
-        }
-        else
-        {
-            string operand1 = st.top();
-            st.pop();
-            string operand2 = st.top();
-            st.pop();
-            string expression = "(" + operand1 + preOrder[i] + operand2 + ")";
-            st.push(expression);
+            for (int j = p1 + 1; j < i; j++)
+            {
+                num1[cnt1] += s1[j];
+            }
+            map1[num1[cnt1++]]++;
+            p1 = i;
         }
     }
-
-    return st.top();
-}
-vector<string> split(const string &s, char delim)
-{
-    stringstream ss(s);
-    string item;
-    vector<string> tokens;
-    while (getline(ss, item, delim))
+    if (s1[s1.size() - 1] != ' ')
     {
-        tokens.push_back(item);
+        for (int j = p1 + 1; j < s1.size(); j++)
+        {
+            num1[cnt1] += s1[j];
+        }
+        map1[num1[cnt1]]++;
     }
-    return tokens;
+    for (int i = 0; i < s2.size(); i++)
+    {
+        if (s2[i] == ' ')
+        {
+            for (int j = p2 + 1; j < i; j++)
+            {
+                num2[cnt2] += s2[j];
+            }
+            if (map1[num2[cnt2]] == 0)
+            {
+                fla[1] = 1;
+            }
+            map1[num2[cnt2++]]--;
+            p2 = i;
+        }
+    }
+    if (s2[s2.size() - 1] != ' ')
+    {
+        for (int j = p2 + 1; j < s2.size(); j++)
+        {
+            num2[cnt2] += s2[j];
+        }
+        if (map1[num2[cnt2]] == 0)
+        {
+            fla[1] = 1;
+        }
+        tmp = map1[num2[cnt2]];
+        map1[num2[cnt2]]--;
+    }
 }
+
+void pre(node *p)
+{
+    if (p == NULL)
+    {
+        return;
+    }
+    char a;
+    a = p->data[0];
+    if (a == '+' || a == '-' || a == '*' || a == '/')
+    {
+        cout << "(";
+    }
+    pre(p->left);
+    if (a == '+' || a == '-' || a == '*' || a == '/')
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << p->data;
+    }
+    pre(p->right);
+    if (a == '+' || a == '-' || a == '*' || a == '/')
+    {
+        cout << ")";
+    }
+}
+
+void solve()
+{
+    getline(cin, s1);
+    getline(cin, s2);
+    readd();
+    if (cnt1 != cnt2)
+    { //数量不相等 
+        cout << "The number of pre-order elements is different from the number of in-order elements.";
+        return;
+    }
+    if (fla[1])
+    { //元素不相同 
+        cout << "Inconsistent pre-order elements and in-order elements.";
+        return;
+    }
+    node *head1;
+    head1 = tree(1, cnt1, 1, cnt1);
+    if (fla[2])
+    {
+        cout << "This binary tree is not exists.";
+        return;
+    }
+    pre(head1->left);
+    cout << num1[1][0];
+    pre(head1->right);
+}
+
 int main()
 {
-    string preOrder, inOrder;
-    getline(cin, preOrder);
-    getline(cin, inOrder);
-    vector<string> preOrderElements = split(preOrder, ' ');
-    vector<string> inOrderElements = split(inOrder, ' ');
-    string expression = convertToExpression(preOrderElements, inOrderElements);
-    cout << expression << endl;
-
+    solve();
     return 0;
 }
