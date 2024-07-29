6480c9a1ce4a14c7dbb43c1e96d9cfdad719ad01
2723573932 <2723573932@qq.com>
2024-07-29 21:30:45 +0800

    写个淡线段树www

--- Transformation.cpp
+++ Transformation.cpp
@@ -3,24 +3,12 @@ using namespace std;
 const int mod = 10007;
 const int N = 100007;
 vector<int> lst(N);
-int qpow(int a, int b)
-{
-    int res = 1;
-    while (b)
-    {
-        if (b & 1)
-            res = res * a % mod;
-        a = a * a % mod;
-        b >>= 1;
-    }
-    return res;
-}
 class sgt
 {
 private:
     struct seg
     {
-        int val, add, mul = 1, set;
+        int val, val2, val3, add, mul = 1, set;
     };
     vector<seg> st;
 
@@ -28,7 +16,9 @@ public:
     sgt() : st(N << 2) {};
     void cal(int node)
     {
-        st[node].val = st[node * 2].val + st[node * 2 + 1].val;
+        st[node].val = (st[node * 2].val + st[node * 2 + 1].val) % mod;
+        st[node].val2 = (st[node * 2].val2 + st[node * 2 + 1].val2) % mod;
+        st[node].val3 = (st[node * 2].val3 + st[node * 2 + 1].val3) % mod;
     }
     void build(int node, int l, int r)
     {
@@ -45,8 +35,19 @@ public:
     void pushdown(int node, int l, int r)
     {
         if (st[node].set)
+        {
             st[node].val = st[node].set * (r - l + 1) % mod;
-        st[node].val = (st[node].val * st[node].mul + st[node].add * (r - l + 1)) % mod;
+            st[node].val2 = st[node].set * st[node].set % mod * (r - l + 1) % mod;
+            st[node].val3 = st[node].set * st[node].set % mod * st[node].set % mod * (r - l + 1) % mod;
+        }
+        st[node].val = (st[node].val * st[node].mul) % mod;
+        st[node].val2 = (st[node].val2 * st[node].mul % mod * st[node].mul) % mod;
+        st[node].val3 = (st[node].val3 * st[node].mul % mod * st[node].mul % mod * st[node].mul) % mod;
+
+        st[node].val3 = (st[node].val3 + 3 * st[node].val2 * st[node].add % mod + 3 * st[node].val * st[node].add % mod * st[node].add % mod + st[node].add * st[node].add % mod * st[node].add % mod * (r - l + 1)) % mod;
+        st[node].val2 = (st[node].val2 + 2 * st[node].val * st[node].add % mod + st[node].add * st[node].add % mod * (r - l + 1)) % mod;
+        st[node].val = (st[node].val + st[node].add * (r - l + 1)) % mod;
+
         if (l < r)
         {
             if (st[node].set)
@@ -59,13 +60,13 @@ public:
             {
                 st[node * 2].mul = (st[node * 2].mul * st[node].mul) % mod;
                 st[node * 2].add = (st[node * 2].add * st[node].mul + st[node].add) % mod;
-                if (st[node * 2].set)
-                    st[node * 2].set = (st[node * 2].set * st[node].mul + st[node].add) % mod;
+                // if (st[node * 2].set)
+                //     st[node * 2].set = (st[node * 2].set * st[node].mul + st[node].add) % mod;
 
                 st[node * 2 + 1].mul = (st[node * 2 + 1].mul * st[node].mul) % mod;
                 st[node * 2 + 1].add = (st[node * 2 + 1].add * st[node].mul + st[node].add) % mod;
-                if (st[node * 2 + 1].set)
-                    st[node * 2 + 1].set = (st[node * 2 + 1].set * st[node].mul + st[node].add) % mod;
+                // if (st[node * 2 + 1].set)
+                //     st[node * 2 + 1].set = (st[node * 2 + 1].set * st[node].mul + st[node].add) % mod;
             }
         }
         st[node].add = 0;
@@ -86,6 +87,8 @@ public:
                 add(node * 2, l, mid, x, y, k);
             if (y > mid)
                 add(node * 2 + 1, mid + 1, r, x, y, k);
+            pushdown(node * 2, l, mid);
+            pushdown(node * 2 + 1, mid + 1, r);
             cal(node);
         }
     }
@@ -96,7 +99,6 @@ public:
         {
             st[node].mul = (st[node].mul * k) % mod;
             st[node].add = (st[node].add * k) % mod;
-            st[node].set = (st[node].set * k) % mod;
         }
         else
         {
@@ -105,6 +107,8 @@ public:
                 mul(node * 2, l, mid, x, y, k);
             if (mid + 1 <= y)
                 mul(node * 2 + 1, mid + 1, r, x, y, k);
+            pushdown(node * 2, l, mid);
+            pushdown(node * 2 + 1, mid + 1, r);
             cal(node);
         }
     }
@@ -124,14 +128,23 @@ public:
                 set(node * 2, l, mid, x, y, k);
             if (mid + 1 <= y)
                 set(node * 2 + 1, mid + 1, r, x, y, k);
+            pushdown(node * 2, l, mid);
+            pushdown(node * 2 + 1, mid + 1, r);
             cal(node);
         }
     }
     int query(int node, int l, int r, int x, int y, int p)
     {
         pushdown(node, l, r);
-        if (l == r)
-            return st[node].val * p % mod;
+        if (x <= l && r <= y)
+        {
+            if (p == 1)
+                return st[node].val;
+            else if (p == 2)
+                return st[node].val2;
+            else
+                return st[node].val3;
+        }
         int mid = (l + r) >> 1;
         int res = 0;
         if (x <= mid)
@@ -140,26 +153,29 @@ public:
             res = (res + query(node * 2 + 1, mid + 1, r, x, y, p)) % mod;
         return res;
     }
-}tree;
+    void rebuild()
+    {
+        fill(st.begin(), st.end(), seg());
+    }
+} tree;
 
 void solve(int n, int m)
 {
-    while(m--)
+    while (m--)
     {
-        int op,x,y,k;
-        cin>>op>>x>>y>>k;
-        if(op==1)
-        tree.add(1,1,n,x,y,k);
-        else if(op==2)
-        tree.mul(1,1,n,x,y,k);
-        else if(op==3)
-        tree.set(1,1,n,x,y,k);
+        int op, x, y, k;
+        cin >> op >> x >> y >> k;
+        if (op == 1)
+            tree.add(1, 1, n, x, y, k);
+        else if (op == 2)
+            tree.mul(1, 1, n, x, y, k);
+        else if (op == 3)
+            tree.set(1, 1, n, x, y, k);
         else
         {
-            cout<<tree.query(1,1,n,x,y,k)<<'\n';
+            cout << tree.query(1, 1, n, x, y, k) << '\n';
         }
     }
-    
 }
 int main()
 {
@@ -167,6 +183,10 @@ int main()
     cin.tie(0);
     int n, m;
     while (cin >> n >> m && n && m)
+    {
         solve(n, m);
+        fill(lst.begin(), lst.end(), 0);
+        tree.rebuild();
+    }
     return 0;
 }
